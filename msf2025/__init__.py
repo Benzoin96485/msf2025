16daaab52429b8252557cbf345581e1f8b6081a9
Weiliang Luo <luowl7@mit.edu>
2025-07-23 10:09:29 -0400

    doc and refactor

--- msf2025/__init__.py
+++ msf2025/__init__.py
@@ -1,7 +1,10 @@
 """A demo package for the MSF Bootcamp 2025."""
 
 # Add imports here
-from .functions import *
-
+from .functions import canvas
+import msf2025.io
+import msf2025.measure
+import msf2025.molecule
+import msf2025.visualize
 
 from ._version import __version__
